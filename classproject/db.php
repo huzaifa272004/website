406b0728fd94361106073eb1274307d3ab5111e2
huzaifa272004 <103019079+huzaifa272004@users.noreply.github.com>
2022-10-26 00:30:03 +0500

    Add files via upload

---
+++ classproject/db.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "project";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die($conn->connect_error);
+}
+echo "";
+?>
